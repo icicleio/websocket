8895b1db8c022ac818ac9b020169bc476cdd4a85
Aaron Piotrowski <aaron@trowski.com>
2016-02-07 23:30:31 -0600

    Add stream parameter on request

--- src/Protocol/Message/WebSocketRequest.php
+++ src/Protocol/Message/WebSocketRequest.php
@@ -3,16 +3,18 @@ namespace Icicle\WebSocket\Protocol\Message;
 
 use Icicle\Http\Message\BasicRequest;
 use Icicle\Http\Message\Uri;
+use Icicle\Stream\ReadableStream;
 
 class WebSocketRequest extends BasicRequest
 {
     /**
      * @param \Icicle\Http\Message\Uri $uri
      * @param string[][] $headers
+     * @param \Icicle\Stream\ReadableStream|null $stream
      * @param string|null $target
      */
-    public function __construct(Uri $uri, array $headers, $target = null)
+    public function __construct(Uri $uri, array $headers, ReadableStream $stream = null, $target = null)
     {
-        parent::__construct('GET', $uri, $headers, null, $target, '1.1');
+        parent::__construct('GET', $uri, $headers, $stream, $target, '1.1');
     }
 }
